cbee3ff16a78c374fc2fc357093d78a4b88750f3
cecilialjungquist <111697322+cecilialjungquist@users.noreply.github.com>
2023-02-02 15:19:24 +0100

    Created getData and renderUI functions

--- js/index.js
+++ js/index.js
@@ -1,19 +1,41 @@
 const BASE_URL = 'https://swapi.dev/api/';
+const ADD_PEOPLE = '/people/'
+let NEW_URL = BASE_URL + ADD_PEOPLE;
+const charactersEl = document.getElementById('characters-list');
 
 async function getData() {
-    let data = await fetch('https://swapi.dev/api/people/');
+    let data = await fetch(NEW_URL);
     data = await data.json();
+    NEW_URL = data.next;
     data = data.results;
     console.log(data);
-
-    data.forEach(element => {
-        console.log(element.name);
-    });
-
-    // // Hämtar nästa sida
-    // data = await fetch(data.next);
-    // data = await data.json();
-    // console.log(data);
+    
+    renderUI(data);
 }
 
 getData();
+
+
+function renderUI(data) {
+    for (let i = 0; i < data.length; i++) {
+        document.querySelector('.characters .loading').classList.add('invisible');
+        let person = document.createElement('li');
+        person.innerHTML = data[i].name;
+        
+        if (!(i % 2 === 0)) {
+            person.style.backgroundColor = '#E1DEDE';
+            person.style.color = '#535556';
+        } else {
+            person.style.backgroundColor = '#8E8E8E';
+            person.style.color = '#FFFFFF';
+        }
+        charactersEl.appendChild(person)
+    }
+}
+
+document.addEventListener('click', () => {
+    charactersEl.innerHTML = '';
+    document.querySelector('.characters .loading').classList.remove('invisible');
+    getData();
+})
+
