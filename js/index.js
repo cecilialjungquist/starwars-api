54b383606da4d0988059f7b236f871bcf505c1d8
cecilialjungquist <111697322+cecilialjungquist@users.noreply.github.com>
2023-02-03 14:05:37 +0100

    updatePage func

--- js/index.js
+++ js/index.js
@@ -8,12 +8,13 @@ const planetEl = document.getElementById('planet-data');
 const nextPage = document.getElementById('next-page');
 const prevPage = document.getElementById('previous-page');
 
-function renderUI(data) {
+function renderPerson(data) {
     for (let i = 0; i < data.length; i++) {
-        document.querySelector('.characters .loading').classList.add('invisible');
-        let person = document.createElement('li');
         let personData = data[i];
-        console.log(personData);
+        // Ta bort loaing gif
+        document.querySelector('.characters .loading').classList.add('invisible');
+        
+        let person = document.createElement('li');
         person.innerHTML = personData.name;
         
         if (!(i % 2 === 0)) {
@@ -26,33 +27,36 @@ function renderUI(data) {
         charactersEl.appendChild(person)
         
         person.addEventListener('click', () => {
-            console.log('you clicked on ' + personData.name);
             detailsEl.innerHTML ='';
             let details = document.createElement('article');
             details.innerHTML = `<h3>${personData.name}</h3>
-            <ul class="person-data-list">
-            <li>Height: ${personData.height} m</li>
-            <li>Mass: ${personData.mass} kg</li>
+                                <ul class="person-data-list">
+                                    <li>Height: ${personData.height} m</li>
+                                    <li>Mass: ${personData.mass} kg</li>
                                     <li>Hair color: ${personData.hair_color}</li>
                                     <li>Skin color: ${personData.skin_color}</li>
                                     <li>Birth year: ${personData.birth_year}</li>
                                     <li>Gender: ${personData.gender}</li>
-                                    </ul>`;
+                                </ul>`;
             detailsEl.appendChild(details);
             renderPlanet(personData.homeworld);
         })
     }
 }
 
+function updatePage() {
+    charactersEl.innerHTML = '';
+    document.querySelector('.characters .loading').classList.remove('invisible');
+    getData();
+    document.getElementById('page-count').innerHTML = pageCount;
+}
+
 async function getData() {
     try {
         let data = await fetch(BASE_URL + ADD_PEOPLE + pageCount);
         data = await data.json();
-        console.log(data);
         data = data.results;
-        console.log(data);
-        
-        renderUI(data);
+        renderPerson(data);
     } catch (error) {
         console.log(error);
     }
@@ -63,18 +67,17 @@ async function renderPlanet(fetchURL) {
         planetEl.innerHTML = '';
         let planetData = await fetch(fetchURL);
         planetData = await planetData.json();
-        console.log(planetData);
         
         let planet = document.createElement('article');
         planet.innerHTML = `<h3>${planetData.name}</h3>
-        <ul class="planet-data-list">
-        <li>Rotation period: ${planetData.rotation_period}h</li>
-        <li>Orbital period: ${planetData.orbital_period} days</li>
-        <li>Diameter: ${planetData.diameter} km</li>
-        <li>Climate: ${planetData.climate}</li>
-        <li>Gravity: ${planetData.gravity}</li>
-        <li>Terrain: ${planetData.terrain}</li>
-        </ul>`;
+                            <ul class="planet-data-list">
+                                <li>Rotation period: ${planetData.rotation_period}h</li>
+                                <li>Orbital period: ${planetData.orbital_period} days</li>
+                                <li>Diameter: ${planetData.diameter} km</li>
+                                <li>Climate: ${planetData.climate}</li>
+                                <li>Gravity: ${planetData.gravity}</li>
+                                <li>Terrain: ${planetData.terrain}</li>
+                            </ul>`;
         planetEl.appendChild(planet);
     } catch (error) {
         console.log(error);
@@ -86,19 +89,13 @@ getData();
 nextPage.addEventListener('click', () => {
     if (pageCount < 9) {
         pageCount++;
-        charactersEl.innerHTML = '';
-        document.querySelector('.characters .loading').classList.remove('invisible');
-        getData();
-        document.getElementById('page-count').innerHTML = pageCount;
+        updatePage();
     }
 })
 
 prevPage.addEventListener('click', () => {
     if (pageCount > 1) {
         pageCount--;
-        charactersEl.innerHTML = '';
-        document.querySelector('.characters .loading').classList.remove('invisible');
-        getData();
-        document.getElementById('page-count').innerHTML = pageCount;
+        updatePage();
     }
 })
