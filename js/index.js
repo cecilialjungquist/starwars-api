23c68634824064cbf46f0491bfaf084519259ca1
cecilialjungquist <111697322+cecilialjungquist@users.noreply.github.com>
2023-02-03 11:26:09 +0100

    Modified nextPage

--- js/index.js
+++ js/index.js
@@ -3,20 +3,21 @@
 // .: Kunna bläddra bakåt i katalogen
 
 const BASE_URL = 'https://swapi.dev/api/';
-const ADD_PEOPLE = '/people/'
-let NEW_URL = BASE_URL + ADD_PEOPLE;
+const ADD_PEOPLE = '/people/?page='
+let pageCount = 1;
 
 const charactersEl = document.getElementById('characters-list');
 const detailsEl = document.getElementById('details-data');
 const planetEl = document.getElementById('planet-data');
 const nextPage = document.getElementById('next-page');
-let pageCount = 1;
+const prevPage = document.getElementById('previous-page');
 
 function renderUI(data) {
     for (let i = 0; i < data.length; i++) {
         document.querySelector('.characters .loading').classList.add('invisible');
         let person = document.createElement('li');
-        let personData = data[i]
+        let personData = data[i];
+        console.log(personData);
         person.innerHTML = personData.name;
         
         if (!(i % 2 === 0)) {
@@ -48,42 +49,60 @@ function renderUI(data) {
 }
 
 async function getData() {
-    let data = await fetch(NEW_URL);
-    data = await data.json();
-    console.log(data);
-    NEW_URL = data.next;
-    data = data.results;
-    console.log(data);
-    
-    renderUI(data);
+    try {
+        let data = await fetch(BASE_URL + ADD_PEOPLE + pageCount);
+        data = await data.json();
+        console.log(data);
+        data = data.results;
+        console.log(data);
+        
+        renderUI(data);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 async function renderPlanet(fetchURL) {
-    planetEl.innerHTML = '';
-    let planetData = await fetch(fetchURL);
-    planetData = await planetData.json();
-    console.log(planetData);
-    
-    let planet = document.createElement('article');
-    planet.innerHTML = `<h3>${planetData.name}</h3>
-    <ul class="planet-data-list">
-    <li>Rotation period: ${planetData.rotation_period}h</li>
-    <li>Orbital period: ${planetData.orbital_period} days</li>
-    <li>Diameter: ${planetData.diameter} km</li>
-    <li>Climate: ${planetData.climate}</li>
-    <li>Gravity: ${planetData.gravity}</li>
-    <li>Terrain: ${planetData.terrain}</li>
-    </ul>`;
-    planetEl.appendChild(planet);
+    try {
+        planetEl.innerHTML = '';
+        let planetData = await fetch(fetchURL);
+        planetData = await planetData.json();
+        console.log(planetData);
+        
+        let planet = document.createElement('article');
+        planet.innerHTML = `<h3>${planetData.name}</h3>
+        <ul class="planet-data-list">
+        <li>Rotation period: ${planetData.rotation_period}h</li>
+        <li>Orbital period: ${planetData.orbital_period} days</li>
+        <li>Diameter: ${planetData.diameter} km</li>
+        <li>Climate: ${planetData.climate}</li>
+        <li>Gravity: ${planetData.gravity}</li>
+        <li>Terrain: ${planetData.terrain}</li>
+        </ul>`;
+        planetEl.appendChild(planet);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 getData();
 
 nextPage.addEventListener('click', () => {
-    pageCount++;
-    charactersEl.innerHTML = '';
-    document.querySelector('.characters .loading').classList.remove('invisible');
-    getData();
-    document.getElementById('page-count').innerHTML = pageCount;
+    if (pageCount < 9) {
+        pageCount++;
+        charactersEl.innerHTML = '';
+        document.querySelector('.characters .loading').classList.remove('invisible');
+        getData();
+        document.getElementById('page-count').innerHTML = pageCount;
+    }
 })
 
+prevPage.addEventListener('click', () => {
+    if (pageCount > 1) {
+        pageCount--;
+        charactersEl.innerHTML = '';
+        document.querySelector('.characters .loading').classList.remove('invisible');
+        getData();
+        document.getElementById('page-count').innerHTML = pageCount;
+    }
+})
